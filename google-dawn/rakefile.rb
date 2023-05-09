a341db1e926f5e679bb9dd897c53862d41abe1b8
peterk <peterk@livingwork.com>
2023-05-09 13:56:28 -0400

    add areas for google dawn webGPU stuff

---
+++ google-dawn/rakefile.rb
@@ -0,0 +1,40 @@
+
+myDir = File.expand_path(File.dirname(__FILE__));
+require "#{myDir}/../build-options.rb"
+require "rakish/GitModule"
+
+depends=[
+]
+
+Rakish.Project(
+    :includes=>[Rakish::CppProjectModule, Rakish::GitModule ],
+    :id          => "",
+	:name 		 => "google-dawn",
+	:dependsUpon => [ depends ]
+) do
+
+	libSource = "#{projectDir}/dawn";
+
+    setSourceSubdir(libSource);
+
+    # readme on building
+    # https://dawn.googlesource.com/dawn/+/refs/heads/chromium-gpu-experimental/README.md
+
+	file libSource do |t|
+	    git.clone("https://dawn.googlesource.com/dawn", t.name );
+	    git.checkout("chromium/4473);
+	end
+
+if(false)
+    export task :cleanAll => sourceSubdir do |t|
+        FileUtils.rm_rf(vendorBuildDir);  # remove recursive
+        FileUtils.cd sourceSubdir do
+            system('git reset --hard');  # Maybe delete and re-download - though a bit slow
+        end
+    end
+end
+    export task :vendorLibs => [ libSource ] do |t|
+    end
+
+end
+
---
+++ google-depot-tools/rakefile.rb
@@ -0,0 +1,37 @@
+
+myDir = File.expand_path(File.dirname(__FILE__));
+require "#{myDir}/../build-options.rb"
+require "rakish/GitModule"
+
+depends=[
+]
+
+Rakish.Project(
+    :includes=>[Rakish::CppProjectModule, Rakish::GitModule ],
+    :id          => "",
+	:name 		 => "google-depot-tools",
+	:dependsUpon => [ depends ]
+) do
+
+	libSource = "#{projectDir}/depot_tools";
+
+    setSourceSubdir(libSource);
+
+    # readme on building
+    # https://dawn.googlesource.com/dawn/+/refs/heads/chromium-gpu-experimental/README.md
+    
+	file libSource do |t|
+        if(targetPlatform =~ /Windows/ )
+  #          FileUtils.mkdir_p(t.name);  # make sure it is there
+            system("curl \"https://storage.googleapis.com/chrome-infra/depot_tools.zip\" -o dptools.zip");
+            system("unzip dptools.zip -d #{t.name}");
+        elsif(targetPlatform =~ /MacOS/)
+	        git.clone("https://chromium.googlesource.com/chromium/tools/depot_tools.git", t.name );
+        end
+	end
+
+    export task :vendorLibs => [ libSource ] do |t|
+    end
+
+end
+
